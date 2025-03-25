d3d8dc0c8f84138899c6d9c61ae71e286c67db1b
gyeongmann <hkc8857@naver.com>
2025-03-25 17:10:01 +0900

    [setting] init project

---
+++ DmailBot/src/main/java/gyeongmann/dmailbot/DmailBotApplication.java
@@ -0,0 +1,13 @@
+package gyeongmann.dmailbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmailBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmailBotApplication.class, args);
+    }
+
+}
---
+++ DmailBot/src/test/java/gyeongmann/dmailbot/DmailBotApplicationTests.java
@@ -0,0 +1,13 @@
+package gyeongmann.dmailbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmailBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
