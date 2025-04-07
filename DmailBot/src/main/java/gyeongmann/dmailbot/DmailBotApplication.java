ed8a7c9318221f678c64e984caacd5bfc4365422
gyeongmann <hkc8857@naver.com>
2025-04-08 00:15:00 +0900

    [debug] build error

--- DmailBot/src/main/java/gyeongmann/dmailbot/DmailBotApplication.java
+++ DmailBot/src/main/java/gyeongmann/dmailbot/DmailBotApplication.java
@@ -1,15 +1,28 @@
 package gyeongmann.dmailbot;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@Slf4j
 public class DmailBotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DmailBotApplication.class, args);
     }
 
+    @PostConstruct
+    public void init() {
+        log.info("DmailBotApplication started successfully!");
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        log.info("DmailBotApplication is shutting down...");
+    }
 }
