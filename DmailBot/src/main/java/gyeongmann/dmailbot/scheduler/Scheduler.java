b4ff4e45101e609a182ca5cb17b260797d5f4ea5
gyeongmann <hkc8857@naver.com>
2025-04-07 23:07:05 +0900

    [fix] set scheduler every minute

--- DmailBot/src/main/java/gyeongmann/dmailbot/scheduler/Scheduler.java
+++ DmailBot/src/main/java/gyeongmann/dmailbot/scheduler/Scheduler.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalTime;
+
 @Component
 @RequiredArgsConstructor
 public class Scheduler {
@@ -13,8 +15,14 @@ public class Scheduler {
     private final Bot bot;
     private final UnreadMailReader unreadMailReader;
 
-    @Scheduled(cron = "0 0 9-18 ? * MON-FRI")
+    @Scheduled(cron = "0 * 9-17 ? * MON-FRI")
     public void run() {
+        LocalTime now = LocalTime.now();
+
+        if (now.getMinute() == 0) {
+            System.out.println("⏰ 정각 실행됨: " + now);
+        }
+
         unreadMailReader.getUnreadMessages(bot);
     }
 }
